3ff954fe8d59f192779a1208cf22304befa3c44f
EmmanuelCruzH <emmanuel_cruzh@ciencias.unam.mx>
2024-07-24 13:08:26 -0600

    Finalizando proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,95 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, onMounted, watch } from "vue";
+import { db } from "./data/guitarras";
+import Guitarra from "./components/Guitarra.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+// const state = reactive({
+//   guitarras: db
+// })
+// console.log(state.guitarras)
+
+const guitarras = ref([]);
+const carrito = ref([]);
+const guitarraPrincipal = ref({});
+
+watch(carrito, () => {
+  guardaCarritoLocal();
+}, 
+{
+  deep: true,
+})
+
+onMounted(() => {
+  guitarras.value = db;
+  guitarraPrincipal.value = db[2];
+  console.count(guitarras.value);
+  const carritoLocal = localStorage.getItem("carrito");
+  if (carritoLocal) carrito.value = JSON.parse(carritoLocal);
+});
+
+const guardaCarritoLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito.value));
+};
+
+const agregarCarrito = (guitarra) => {
+  const existeCarrito = carrito.value.findIndex(
+    (producto) => producto.id === guitarra.id
+  );
+  console.log(guitarra);
+  if (existeCarrito < 0) {
+    guitarra.cantidad = 1;
+    carrito.value.push(guitarra);
+  } else {
+    carrito.value[existeCarrito].cantidad++;
+  }
+};
+
+const setCantidad = (guitarra, newCantidad) => {
+  const indexGuitarra = carrito.value.findIndex(
+    (producto) => producto.id === guitarra.id
+  );
+  if (newCantidad >= 1) carrito.value[indexGuitarra].cantidad = newCantidad;
+};
+
+const eliminarDeCarrito = (guitarra) => {
+  const indexGuitarra = carrito.value.findIndex(
+    (producto) => producto.id === guitarra.id
+  );
+  carrito.value.splice(indexGuitarra, 1);
+};
+
+const vaciarCarrito = () => {
+  carrito.value = [];
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header
+    :carrito="carrito"
+    :guitarraPrincipal="guitarraPrincipal"
+    @set-cantidad="setCantidad"
+    @eliminar-de-carrito="eliminarDeCarrito"
+    @agregar-carrito="agregarCarrito"
+    @vaciar-carrito="vaciarCarrito"
+  />
+
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+
+    <div class="row mt-5">
+      <Guitarra
+        v-for="guitarra in guitarras"
+        :key="guitarra.id"
+        :guitarra="guitarra"
+        @agregar-carrito="agregarCarrito"
+      />
+    </div>
+  </main>
+
+  <Footer />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
